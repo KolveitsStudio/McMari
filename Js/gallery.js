b31d3228a0a980854d761342b641d83813771e4a
devfilmbase <dev.filmbase@gmail.com>
2026-01-11 15:27:44 +0100

    color changes dynamic for phone and web

--- Js/gallery.js
+++ Js/gallery.js
@@ -19,9 +19,9 @@ function populateGallery(){
 
   gallery.innerHTML = galleryArr.map((x, i)=>
     `
-    <div class="gallery-item d-flex" style="animation-delay:${i*80}ms">
-      <img class="d-block bg-white" src="${x.src}" alt="${x.alt}">
-      <div class="caption d-flex align-items-center justify-content-center bg-white"><p class="my-auto">${x.caption}</p></div>
+    <div class="gallery-item d-flex rounded-2" style="animation-delay:${i*80}ms">
+      <img class="d-block" src="${x.src}" alt="${x.alt}">
+      <div class="caption d-flex align-items-center justify-content-center "><p class="my-auto">${x.caption}</p></div>
     </div>
     `
   ).join("")
